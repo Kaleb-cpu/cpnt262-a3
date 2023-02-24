5d286f776f9ec4e25dc2f304e97b8b89dfab77c6
Kaleb berhane <kakuj424@gmail.com>
2023-02-24 02:21:06 -0700

    else statement added

--- js/app.js
+++ js/app.js
@@ -28,83 +28,79 @@ const handleSubmit = function(event) {
   // Using if, else if and else blocks: assign a meal recommendation to `answer` based on the submitted options for `day` and `meal`.
 
   // Monday Meals: 
-  if (day === 'mon' && meal === 'breakfast') {
-    answer = 'Sausages'; 
-  }
-  if ( meal === 'lunch'){
+  if (day === 'mon' && meal === 'breakfast')
+    answer = 'Have some Avocado Toast to kick off this week';
+  else if (day === 'mon' && meal === 'lunch')
     answer = 'Grilled Chicken Salad';
-  }
-  if ( meal === 'dinner'){
-    answer = 'Faijta Pasta';
-  }
-
+  else if (day === 'mon' && meal === 'dinner')
+    answer = 'Faijta Pasta are best for Monday Supper';
+  else answer = 'Please select both options'
+    
   //  Tuesday Meals
-  if (day === 'tue' && meal === 'breakfast') {
+  if (day === 'tue' && meal === 'breakfast')
     answer = 'French Toast'; 
-  }
-  if ( meal === 'lunch'){
-    answer = 'Roast Beef Sandwich'
-  }
-  if ( meal === 'dinner'){
+  else if (day === 'tue' && meal === 'lunch')
+    answer = 'You would love Roast Beef Sandwich on a Tuesday afternoon'
+  else if (day === 'tue' && meal === 'dinner')
     answer = 'Roasted Cauliflower Tacos'
-  }
-
+  
   //  Wednesday Meals
-  if (day === 'wed' && meal === 'breakfast') {
-    answer = 'Cereal'; 
-  }
-  if ( meal === 'lunch'){
+  if (day === 'wed' && meal === 'breakfast')
+    answer = 'Sausages are the best mid week breakfast'; 
+  else if (day === 'wed' && meal === 'lunch')
     answer = 'Vegetarian Burrito'
-  }
-  if ( meal === 'dinner'){
+  else if (day === 'wed' && meal === 'dinner')
     answer = 'Minestrone Soup'
-  }
-
+  
   // Thursday Meals
-  if (day === 'thu' && meal === 'breakfast') {
+  if (day === 'thu' && meal === 'breakfast')
     answer = 'Bacon'; 
-  }
-  if ( meal === 'lunch'){
+  else if (day === 'thu' && meal === 'lunch')
     answer = 'Chicken Quesadillas'
-  }
-  if ( meal === 'dinner'){
-    answer = 'Bean Chili'
-  }
+  else if (day === 'thu' && meal === 'dinner')
+    answer = 'Bean Chilis on a Thursday night are the best'
 
   // Friday Meals
-  if (day === 'fri' && meal === 'breakfast') {
-    answer = 'Avocado Toast'; 
-  }
-  if ( meal === 'lunch'){
+  if (day === 'fri' && meal === 'breakfast')
+    answer = 'You gotta have Cereal for a Friday Morning'; 
+  if (day === 'fri' && meal === 'lunch')
     answer = 'Frittata'
-  }
-  if ( meal === 'dinner'){
+  else if (day === 'fri' && meal === 'dinner')
     answer = 'Sesame Tofu'
-  }
-
+    
   // Saturday Meals
-  if (day === 'sat' && meal === 'breakfast') {
+  if (day === 'sat' && meal === 'breakfast')
     answer = 'Chicken and Waffles'; 
-  }
-  if ( meal === 'lunch'){
+  else if (day === 'sat' && meal === 'lunch')
     answer = 'Hummus and Veggie Sandwich'
-  }
-  if ( meal === 'dinner'){
+  else if (day === 'sat' && meal === 'dinner')
     answer = 'Stuffed Shells'
-  }
-
+    
   // Sunday Meals
-  if (day === 'sun' && meal === 'breakfast') {
-    answer = 'Pancakes'; 
-  }
-  if ( meal === 'lunch'){
-    answer = 'Tomato Soup and Grilled Cheese'
-  }
-  if ( meal === 'dinner'){
-    answer = 'Baked Feta'
-  }
-
-  
+  if (day === 'sun' && meal === 'breakfast')
+      answer = 'Pancakes'; 
+  else if (day === 'sun' && meal === 'lunch')
+      answer = 'Tomato Soup and Grilled Cheese'
+  else if (day === 'sun' && meal === 'dinner')
+      answer = 'Baked Feta'
+    
+  // If you need 7 different meal recommendations based on at least one form option, the code would look like this:
+  // if (day === 'mon')
+  //   answer = 'Sausages';
+  // if (day === 'tue')
+  //   answer = 'French Toast'; 
+  // if (day === 'wed')
+  //   answer = 'Cereal';
+  // if (day === 'thu')
+  //   answer = 'Bacon';
+  // if (day === 'fri')
+  //   answer = 'Avocado Toast';
+  // if (day === 'sat')
+  //   answer = 'Chicken and Waffles';
+  // if (day === 'sun')
+  //   answer = 'Pancakes';
+  //   else answer = 'Please select both Options'
+    
   
   
 
