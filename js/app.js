2eab7ec2a174440e3ee8392952189a625f19e73b
Kaleb berhane <kakuj424@gmail.com>
2023-02-23 23:58:37 -0700

    meal of the day added for whole week

--- js/app.js
+++ js/app.js
@@ -27,10 +27,86 @@ const handleSubmit = function(event) {
 
   // Using if, else if and else blocks: assign a meal recommendation to `answer` based on the submitted options for `day` and `meal`.
 
-  // For example: 
-  if (day === 'tue') {
-    answer = 'tacos';
+  // Monday Meals: 
+  if (day === 'mon' && meal === 'breakfast') {
+    answer = 'Sausages'; 
   }
+  else if ( meal === 'lunch'){
+    answer = 'Grilled Chicken Salad'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Faijta Pasta'
+  }
+
+  //  Tuesday Meals
+  if (day === 'tue' && meal === 'breakfast') {
+    answer = 'French Toast'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Roast Beef Sandwich'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Roasted Cauliflower Tacos'
+  }
+
+  //  Wednesday Meals
+  if (day === 'wed' && meal === 'breakfast') {
+    answer = 'Cereal'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Vegetarian Burrito'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Minestrone Soup'
+  }
+
+  // Thursday Meals
+  if (day === 'thu' && meal === 'breakfast') {
+    answer = 'Bacon'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Chicken Quesadillas'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Bean Chili'
+  }
+
+  // Friday Meals
+  if (day === 'fri' && meal === 'breakfast') {
+    answer = 'Avocado Toast'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Frittata'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Sesame Tofu'
+  }
+
+  // Saturday Meals
+  if (day === 'sat' && meal === 'breakfast') {
+    answer = 'Chicken and Waffles'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Hummus and Veggie Sandwich'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Stuffed Shells'
+  }
+
+  // Sunday Meals
+  if (day === 'sun' && meal === 'breakfast') {
+    answer = 'Pancakes'; 
+  }
+  else if ( meal === 'lunch'){
+    answer = 'Tomato Soup and Grilled Cheese'
+  }
+  else if ( meal === 'dinner'){
+    answer = 'Baked Feta'
+  }
+
+  
+  
+  
 
 
 
